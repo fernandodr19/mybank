d910204073a84d16bbd9daa264a11004335e8a64
Fernando Da Rós <fernando.ros@stone.com.br>
2021-07-24 05:28:41 -0300

    test: add db test cases

--- pkg/tests/db_test.go
+++ pkg/tests/db_test.go
@@ -10,21 +10,52 @@ import (
 
 func Test_TransactionSave(t *testing.T) {
 	testTable := []struct {
-		Name string
-		Tx   entities.Transaction
+		Name        string
+		Tx          entities.Transaction
+		ExpectError bool
 	}{
 		{
 			Name: "save tx happy path",
 			Tx: entities.Transaction{
 				AccountID: "79924661-86b6-4160-bd9e-48f3fbc4b489",
-				Operaion:  1,
+				Operaion:  4,
 				Amount:    100,
 			},
+		},
+		{
+			Name: "save negative amount tx happy path",
+			Tx: entities.Transaction{
+				AccountID: "79924661-86b6-4160-bd9e-48f3fbc4b489",
+				Operaion:  1,
+				Amount:    -100,
+			},
+		},
+		{
+			Name: "invalid acc id uuid",
+			Tx: entities.Transaction{
+				AccountID: "123",
+				Operaion:  1,
+				Amount:    -100,
+			},
+			ExpectError: true,
+		},
+		{
+			Name: "fk violation, invalid operation",
+			Tx: entities.Transaction{
+				AccountID: "79924661-86b6-4160-bd9e-48f3fbc4b489",
+				Operaion:  -1,
+				Amount:    -100,
+			},
+			ExpectError: true,
 		},
 	}
 	for _, tt := range testTable {
 		t.Run(tt.Name, func(t *testing.T) {
 			txID, err := testEnv.TxRepo.SaveTransaction(context.Background(), tt.Tx)
+			if tt.ExpectError {
+				assert.Error(t, err)
+				return
+			}
 			assert.NoError(t, err)
 			assert.NotEmpty(t, txID)
 		})

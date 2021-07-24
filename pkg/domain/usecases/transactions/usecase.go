65051c7d3b7e504a55a83802820d3872289baaf1
Fernando Da Rós <fernando.ros@stone.com.br>
2021-07-24 08:10:44 -0300

    docs: describe some exported funcs

--- pkg/domain/usecases/transactions/usecase.go
+++ pkg/domain/usecases/transactions/usecase.go
@@ -7,6 +7,7 @@ import (
 	"github.com/fernandodr19/mybank-tx/pkg/domain/vos"
 )
 
+// AccountsClient responsible for communicating with accounts server
 type AccountsClient interface {
 	Deposit(ctx context.Context, accID vos.AccountID, amount vos.Money) error
 	Withdrawal(ctx context.Context, accID vos.AccountID, amount vos.Money) error
@@ -18,11 +19,13 @@ type Repository interface {
 	SaveTransaction(context.Context, entities.Transaction) (vos.TransactionID, error)
 }
 
+// Usecase of transactions
 type Usecase struct {
 	transactionsRepo Repository
 	accountsClient   AccountsClient
 }
 
+// NewUsecase builds a tx usecase
 func NewUsecase(txRepo Repository, accClient AccountsClient) *Usecase {
 	return &Usecase{
 		transactionsRepo: txRepo,
